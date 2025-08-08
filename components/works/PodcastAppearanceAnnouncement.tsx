bc0dc7bad3e387ba4f2e19b12f8808d1e5acf49f
evwalt <ewpw5678@gmail.com>
2025-08-08 14:06:40 -0700

    feat: reword podcast appearance

--- components/works/PodcastAppearanceAnnouncement.tsx
+++ components/works/PodcastAppearanceAnnouncement.tsx
@@ -13,8 +13,8 @@ export default function PodcastAppearanceAnnouncement({
   return (
     <Work addClassNames={addClassNames}>
       <p>
-        I am excited to announce that I had the privilege of appearing as a
-        guest on{' '}
+        I am excited to announce that I had the privilege of appearing twice as
+        a guest on{' '}
         <a
           href='https://podcast.thereactshow.com/1764837/12142504'
           className={textLinkColors}
@@ -25,12 +25,10 @@ export default function PodcastAppearanceAnnouncement({
         </a>{' '}
         podcast!
       </p>
-      <p>On the show, I...</p>
-      <ul className='list-outside list-disc px-6'>
-        <li>Share my journey into software engineering</li>
-        <li>Encourage new engineers</li>
-        <li>Discuss topics related to React</li>
-      </ul>
+      <p>
+        On the show, I share my software engineering journey and insights on{' '}
+        <b>React</b>, <b>developer tools</b>, and <b>industry growth</b>.
+      </p>
     </Work>
   )
 }
