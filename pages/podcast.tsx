874a0339298a979c51b2e1ea160dafc8b150d203
evwalt <ewpw5678@gmail.com>
2025-08-08 15:00:55 -0700

    chore: format podcast page

--- pages/podcast.tsx
+++ pages/podcast.tsx
@@ -1,11 +1,12 @@
 import PodcastAppearanceAnnouncement from 'components/works/PodcastAppearanceAnnouncement'
-import PageReference from 'components/PageReference'
 
 export default function podcast() {
   return (
     <div className='flex flex-col gap-y-3 text-lg'>
       <h1 className='text-3xl font-bold sm:text-4xl'>Podcast</h1>
-      <PodcastAppearanceAnnouncement addClassNames={'from-blue-500 via-purple-500 to-pink-500'} />
+      <PodcastAppearanceAnnouncement
+        addClassNames={'from-blue-500 via-purple-500 to-pink-500'}
+      />
     </div>
   )
 }
