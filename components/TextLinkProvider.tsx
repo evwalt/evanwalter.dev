8289d21624aab51a16c0a9837ac25fad7d845562
evwalt <ewpw5678@gmail.com>
2025-08-08 14:23:56 -0700

    feat: update text link hover colors

--- components/TextLinkProvider.tsx
+++ components/TextLinkProvider.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from 'react'
 
 const TextLinkContext = createContext<string>(
-  'font-medium text-blue-500 underline hover:text-black dark:text-blue-400 dark:hover:text-white'
+  'font-medium text-blue-500 underline hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-300',
 )
 
 export function useTextLinkContext() {
